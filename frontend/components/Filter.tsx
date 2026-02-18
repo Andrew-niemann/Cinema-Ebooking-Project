c2e6bf39dd4c0f803fc9ab84211b377ac6f9b0b9
Alex Greenfield <cag89554@uga.edu>
2026-02-18 16:52:44 -0500

    Created Base UI Prototype

---
+++ frontend/components/Filter.tsx
@@ -0,0 +1,18 @@
+// components/Filter.tsx
+"use client";
+
+
+import { useState } from "react";
+
+export default function Navbar() {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    return (
+        <button className="absolute inset-y-2 right-2 bg-[#ECDFCC] text-[#1E201E] px-6 rounded-full font-semibold hover:bg-white transition-colors"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        >
+            Filter
+        </button>
+    )
+}
