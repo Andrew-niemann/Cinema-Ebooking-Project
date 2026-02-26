e74bbad435177dfae592aae980861b5a5bbf68c8
Matt Bass <mattsbass1110@gmail.com>
2026-02-26 18:43:18 -0500

    Search bar working

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,55 +1,117 @@
-import CardRow from '@/components/CardRow';
-import Search from '@/components/Search';
+"use client";
+
+import { useEffect, useState } from "react";
+import CardRow from "@/components/CardRow";
+import Search from "@/components/Search";
 
 type Movie = {
-    id: number;
-    title: string;
-    posterUrl: string;
-    trailerUrl: string;
-    genre: string;
-    rating: string;
-    description: string;
-    status: string;
-    showings: string;
+  id: number;
+  title: string;
+  posterUrl: string;
+  trailerUrl: string;
+  genre: string;
+  rating: string;
+  description: string;
+  status: string;
+  showings: string;
 };
 
+export default function Home() {
 
-export default async function Home() {
-  
-  const response = await fetch('http://localhost:8080/api/movies', { 
-      cache: 'no-store' 
-  });
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [searchResults, setSearchResults] = useState<Movie[]>([]);
+  const [hasSearched, setHasSearched] = useState(false);
 
-  // Parse the JSON response into an array of Movie objects
-  const movies: Movie[] = await response.json();
+  // Fetch movies from backend
+  useEffect(() => {
+    fetch("http://localhost:8080/api/movies", {
+      cache: "no-store",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
 
-  // Filter movies by genre
-  const currentlyShowingMovies = movies.filter(movie => movie.status.includes('Currently Running'));
-  const comingSoonMovies = movies.filter(movie => movie.status.includes('Coming Soon'));
-  const crimeMovies = movies.filter(movie => movie.genre.includes('Crime'));
-  const dramaMovies = movies.filter(movie => movie.genre.includes('Drama'));
-  const actionMovies = movies.filter(movie => movie.genre.includes('Action'));
-  const adventureMovies = movies.filter(movie => movie.genre.includes('Adventure'));
-  const fantasyMovies = movies.filter(movie => movie.genre.includes('Fantasy'));
-  const romanceMovies = movies.filter(movie => movie.genre.includes('Romance'));
-  const sciFiMovies = movies.filter(movie => movie.genre.includes('Sci-Fi'));
-  const animationMovies = movies.filter(movie => movie.genre.includes('Animation'));
-  
-  // Return homepage with movie data
+  // Search handler
+  const handleSearch = (searchTerm: string, filter: string) => {
+    setHasSearched(true);
+
+    let filtered = movies;
+
+    if (filter === "No Filter") {
+      filtered = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    if (filter === "Genre") {
+      filtered = movies.filter((movie) =>
+        movie.genre.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    if (filter === "Date") {
+      filtered = movies.filter((movie) =>
+        movie.showings.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    setSearchResults(filtered);
+  };
+
+  // Filter movies by genre and status
+  const currentlyShowingMovies = movies.filter((movie) =>
+    movie.status.includes("Currently Running")
+  );
+  const comingSoonMovies = movies.filter((movie) =>
+    movie.status.includes("Coming Soon")
+  );
+  const crimeMovies = movies.filter((movie) =>
+    movie.genre.includes("Crime")
+  );
+  const dramaMovies = movies.filter((movie) =>
+    movie.genre.includes("Drama")
+  );
+  const actionMovies = movies.filter((movie) =>
+    movie.genre.includes("Action")
+  );
+  const adventureMovies = movies.filter((movie) =>
+    movie.genre.includes("Adventure")
+  );
+  const fantasyMovies = movies.filter((movie) =>
+    movie.genre.includes("Fantasy")
+  );
+  const romanceMovies = movies.filter((movie) =>
+    movie.genre.includes("Romance")
+  );
+  const sciFiMovies = movies.filter((movie) =>
+    movie.genre.includes("Sci-Fi")
+  );
+  const animationMovies = movies.filter((movie) =>
+    movie.genre.includes("Animation")
+  );
+
   return (
     <main className="min-h-screen bg-[#1E201E] flex-column items-center justify-center p-8">
-      
-      <Search />
-      <CardRow genre="Currently Showing" movies={currentlyShowingMovies}/>
-      <CardRow genre="Coming Soon" movies={comingSoonMovies}/>
-      <CardRow genre="Crime" movies={crimeMovies}/>
-      <CardRow genre="Drama" movies={dramaMovies}/>
-      <CardRow genre="Action" movies={actionMovies}/>
-      <CardRow genre="Adventure" movies={adventureMovies}/>
-      <CardRow genre="Fantasy" movies={fantasyMovies}/>
-      <CardRow genre="Romance" movies={romanceMovies}/>
-      <CardRow genre="Sci-Fi" movies={sciFiMovies}/>
-      <CardRow genre="Animation" movies={animationMovies}/>
+
+      <Search onSearch={handleSearch} />
+
+      {/* Show Search Results if user searched */}
+      {hasSearched && (
+        <CardRow genre="Search Results" movies={searchResults} />
+      )}
+
+      <CardRow genre="Currently Showing" movies={currentlyShowingMovies} />
+      <CardRow genre="Coming Soon" movies={comingSoonMovies} />
+      <CardRow genre="Crime" movies={crimeMovies} />
+      <CardRow genre="Drama" movies={dramaMovies} />
+      <CardRow genre="Action" movies={actionMovies} />
+      <CardRow genre="Adventure" movies={adventureMovies} />
+      <CardRow genre="Fantasy" movies={fantasyMovies} />
+      <CardRow genre="Romance" movies={romanceMovies} />
+      <CardRow genre="Sci-Fi" movies={sciFiMovies} />
+      <CardRow genre="Animation" movies={animationMovies} />
 
     </main>
   );
--- frontend/components/Search.tsx
+++ frontend/components/Search.tsx
@@ -3,20 +3,30 @@
 
 import { useState } from "react";
 
-export default function Search() {
+type SearchProps = {
+    onSearch: (searchTerm: string, filter: string) => void;
+};
+
+export default function Search({ onSearch }: SearchProps) {
 
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const [filterApplied, setFilter] = useState("No Filter")
+    const [filterApplied, setFilter] = useState("No Filter");
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearch = () => {
+        onSearch(searchTerm, filterApplied);
+    };
 
     return (
-        <div className="inline-flex items-center justify-start rounded-full bg-[#3C3D37]">
+        <div className="inline-flex items-center justify-start rounded-full bg-[#3C3D37] relative">
 
             {/* Search Icon */}
             <div className="flex absolute pl-4">
-                <svg className="w-6 h-6 text-[#697565]"
-                    fill="none" 
-                    stroke="currentColor" 
-                    viewBox="0 0 24 24" 
+                <svg
+                    className="w-6 h-6 text-[#697565]"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg"
                 >
                     <path
@@ -29,19 +39,33 @@ export default function Search() {
             </div>
 
             {/* Input Field */}
-            <input className="w-120 py-4 p-12 text-lg text-[#ECDFCC] focus:outline-none"
-                type="text" 
-                placeholder="Search for a movie..." 
+            <input
+                className="w-120 py-4 p-12 text-lg text-[#ECDFCC] focus:outline-none"
+                type="text"
+                placeholder="Search for a movie..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        handleSearch();
+                    }
+                }}
             />
 
             {/* Search Button */}
-            <button className="p-2 mx-2 inset-y-2 right-2 bg-[#697565] text-[#ECDFCC] px-6 rounded-full font-semibold hover:bg-white hover:text-[#1E201E] transition-colors">
+            <button
+                className="p-2 mx-2 mr-4 inset-y-2 right-2 bg-[#697565] text-[#ECDFCC] px-6 rounded-full font-semibold hover:bg-white hover:text-[#1E201E] transition-colors"
+                onClick={() => {
+                    handleSearch();
+                }}
+            >
                 Search
             </button>
 
             {/* Filter Button */}
             <div className="relative">
-                <button className="p-2 mx-2 mr-4 inset-y-2 right-2 bg-[#697565] text-[#ECDFCC] px-6 rounded-full font-semibold hover:bg-white hover:text-[#1E201E] transition-colors"
+                <button
+                    className="p-2 mx-2 mr-4 inset-y-2 right-2 bg-[#697565] text-[#ECDFCC] px-6 rounded-full font-semibold hover:bg-white hover:text-[#1E201E] transition-colors"
                     onClick={() => setIsDropdownOpen(!isDropdownOpen)}
                 >
                     {filterApplied}
@@ -49,28 +73,33 @@ export default function Search() {
 
                 {/* Genre Dropdown Menu */}
                 {isDropdownOpen && (
-                    <div className="absolute rounded bg-[#697565]">
-                        <button className="block w-full text-left px-4 py-2 text-sm text-[#ECDFCC] duration-200 hover:bg-gray-100 hover:text-[#1E201E]"
-                        onClick={() => {
-                            setIsDropdownOpen(false);
-                            setFilter("No Filter")
-                        }}
+                    <div className="absolute rounded bg-[#697565] mt-2 w-full">
+                        <button
+                            className="block w-full text-left px-4 py-2 text-sm text-[#ECDFCC] duration-200 hover:bg-gray-100 hover:text-[#1E201E]"
+                            onClick={() => {
+                                setIsDropdownOpen(false);
+                                setFilter("No Filter");
+                            }}
                         >
                             No Filter
                         </button>
-                        <button className="block w-full text-left px-4 py-2 text-sm text-[#ECDFCC] duration-200 hover:bg-gray-100 hover:text-[#1E201E]"
-                        onClick={() => {
-                            setIsDropdownOpen(false);
-                            setFilter("Genre")
-                        }}
+
+                        <button
+                            className="block w-full text-left px-4 py-2 text-sm text-[#ECDFCC] duration-200 hover:bg-gray-100 hover:text-[#1E201E]"
+                            onClick={() => {
+                                setIsDropdownOpen(false);
+                                setFilter("Genre");
+                            }}
                         >
                             Genre
                         </button>
-                        <button className="block w-full text-left px-4 py-2 text-sm text-[#ECDFCC] duration-200 hover:bg-gray-100 hover:text-[#1E201E]"
-                        onClick={() => {
-                            setIsDropdownOpen(false);
-                            setFilter("Date")
-                        }}
+
+                        <button
+                            className="block w-full text-left px-4 py-2 text-sm text-[#ECDFCC] duration-200 hover:bg-gray-100 hover:text-[#1E201E]"
+                            onClick={() => {
+                                setIsDropdownOpen(false);
+                                setFilter("Date");
+                            }}
                         >
                             Date
                         </button>
@@ -78,5 +107,5 @@ export default function Search() {
                 )}
             </div>
         </div>
-    )
+    );
 }
